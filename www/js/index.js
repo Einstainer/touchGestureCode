41db8040379b3046136fa38324c42dd8ea9b72f9
Einstainer <rana2_15@uni.worc.ac.uk>
2017-11-06 16:47:57 +0000

    Changing pages

--- www/js/index.js
+++ www/js/index.js
@@ -10,13 +10,46 @@ $(document).on("pagecreate","#pageone",function(){
     	$(this).css('color', 'red');
  	}); 
 
-	$('#swipetext').on("swipeleft",function(){
-    	$(this).css('color', 'green');
-  	});
     
     $('#swipetext').on("swiperight",function(){
     	$(this).css('color', 'blue');
   	});
+    
+    $('#pageone').on("swipeleft",function(){
+        if($.event.special.swipe.start.origin == $('#swipetext')){
+            $(this).css('color', 'green');
+        }
+        
+        else{
+            $.mobile.navigate( "#pagetwo" );
+        }
+    });
+});
+
+$(document).on("pagecreate","#pagetwo",function(){
+  
+  	$('#tapholdtext').on("taphold",function(){
+    	$(this).hide();
+ 	});                       
+
+	$('#taptext').on("tap",function(){
+    	$(this).css('color', 'red');
+ 	}); 
+
+	$('#swipetext').on("swipeleft",function(){
+    	$(this).css('color', 'green');
+  	});
+
+    
+    $('#pageone').on("swiperight",function(){
+        if($.event.special.swipe.start.origin == $('#swipetext')){
+            $(this).css('color', 'green');
+        }
+        
+        else{
+            $.mobile.navigate( "#pageone" );
+        }
+    });
 
 
 });
